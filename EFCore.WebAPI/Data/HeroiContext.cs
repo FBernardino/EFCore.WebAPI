eacdfaeea647265f1a4dc2cf3071555127b158da
Filipe Bernardino <filipebernardinofaria@gmail.com>
2020-09-21 21:33:37 -0300

    Criacao do Contexto | Entidades do Contexto

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.WbAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=admin123;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-CVK2GOE");
+        }
+    }
+}
